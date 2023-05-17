636859c403fd60a144bfe826bf8a765cb58c914d
YeJun, Jung <yejun614@naver.com>
2023-05-17 20:47:17 +0900

    Initial commit

---
+++ server/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HelloWorldController : ControllerBase
+{
+    private readonly ILogger<HelloWorldController> mLogger;
+
+    public HelloWorldController(ILogger<HelloWorldController> logger)
+    {
+        mLogger = logger;
+    }
+
+    [HttpGet]
+    public string Get()
+    {
+        mLogger.LogInformation($"Hello");
+
+        return "Hello, World!";
+    }
+}
+
