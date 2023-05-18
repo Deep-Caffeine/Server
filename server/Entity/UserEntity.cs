902a6e85dbe17c5202548dad42d8c2f487929939
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-19 00:03:12 +0900

    [Feat] UserEntity 추가

---
+++ server/Entity/UserEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace server.Entites
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Username { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Birth { get; set; }
+
+        public string Profile_url { get; set; }
+
+        public long Level { get; set; }
+
+        public string Sns { get; set; }
+
+        public UserEntity()
+        {
+            Email = "";
+            Password = "";
+            Username = "";
+            Phone = "";
+            Birth = "";
+            Profile_url = "";
+            Level = 0;
+            Sns = "";
+        }
+    }
+}
