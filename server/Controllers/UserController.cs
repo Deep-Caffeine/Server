e319cf2aea1eb968e591b6756c4adcb0aac25c3b
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-19 00:20:38 +0900

    [Feat] UserController 추가

---
+++ server/Controllers/UserController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+using server.Entites;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserEntity>>> GetUsers()
+        {
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserEntity>> GetUserEntity(long id)
+        {
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
+            var userEntity = await _context.Users.FindAsync(id);
+
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+
+            return userEntity;
+        }
+
+        // PUT: api/User/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserEntity(long id, UserEntity userEntity)
+        {
+            if (id != userEntity.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userEntity).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserEntityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/User
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserEntity>> PostUserEntity(UserEntity userEntity)
+        {
+          if (_context.Users == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Users'  is null.");
+          }
+            _context.Users.Add(userEntity);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserEntity", new { id = userEntity.Id }, userEntity);
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserEntity(long id)
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+            var userEntity = await _context.Users.FindAsync(id);
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(userEntity);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserEntityExists(long id)
+        {
+            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
