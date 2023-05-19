576aa174af07b6781c3170bb918a1119dfcc93f4
StarJune <partis98@kakao.com>
2023-05-20 01:31:00 +0900

    [fix] DbContext 서비스 등록

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,9 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using server.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
