f297032e6ce39ded6ce0c2b62ca7f90b80c19f5f
StarJune <partis98@kakao.com>
2023-05-20 02:46:17 +0900

    [Feat] User 더미 데이터 반환 구현
    -

--- server/Controllers/UserController.cs
+++ server/Controllers/UserController.cs
@@ -6,11 +6,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using server.Data;
+using server.Data.DTOs;
 using server.Entites;
 
 namespace server.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("user")]
     [ApiController]
     public class UserController : ControllerBase
     {
@@ -21,99 +22,20 @@ namespace server.Controllers
             _context = context;
         }
 
-        // GET: api/User
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<UserEntity>>> GetUsers()
+        public async Task<ActionResult<GetUserResponse>> Get()
         {
-          if (_context.Users == null)
-          {
-              return NotFound();
-          }
-            return await _context.Users.ToListAsync();
-        }
-
-        // GET: api/User/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<UserEntity>> GetUserEntity(long id)
-        {
-          if (_context.Users == null)
-          {
-              return NotFound();
-          }
-            var userEntity = await _context.Users.FindAsync(id);
-
-            if (userEntity == null)
+            GetUserResponse getUserResponse = new GetUserResponse
             {
-                return NotFound();
-            }
-
-            return userEntity;
-        }
-
-        // PUT: api/User/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUserEntity(long id, UserEntity userEntity)
-        {
-            if (id != userEntity.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(userEntity).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserEntityExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/User
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<UserEntity>> PostUserEntity(UserEntity userEntity)
-        {
-          if (_context.Users == null)
-          {
-              return Problem("Entity set 'ApplicationDbContext.Users'  is null.");
-          }
-            _context.Users.Add(userEntity);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetUserEntity", new { id = userEntity.Id }, userEntity);
-        }
-
-        // DELETE: api/User/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUserEntity(long id)
-        {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
-            var userEntity = await _context.Users.FindAsync(id);
-            if (userEntity == null)
-            {
-                return NotFound();
-            }
-
-            _context.Users.Remove(userEntity);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+                email = "von0401@deu.ac.kr",
+                username = "Eun Jung Von",
+                phone = "010-1234-5678",
+                birth = "2014-04-01",
+                profile_url = "/image/trolls",
+                level = 418,
+                sns = new string[3] { "kakao", "naver", "google" }
+            };
+            return Ok(getUserResponse);
         }
 
         private bool UserEntityExists(long id)
