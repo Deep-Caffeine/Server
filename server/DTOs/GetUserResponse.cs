51816bb25d3ff435126a0f3d5ec6fe798b5d3a3d
StarJune <partis98@kakao.com>
2023-05-20 02:40:44 +0900

    [Feat] GetUserResponse DTO 생성
    User 정보를 반환하기 위한 객체

---
+++ server/DTOs/GetUserResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace server.Data.DTOs;
+
+public class GetUserResponse
+{
+    public string email { get; set; }
+    public string username { get; set; }
+    public string phone { get; set; }
+    public string birth { get; set; }
+    public string profile_url { get; set; }
+    public long level { get; set; }
+    public string[] sns { get; set; }
+}
