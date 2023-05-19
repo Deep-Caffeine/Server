8c32ab224c7cffb102d2fd9549ac83e3a225ad4c
StarJune <partis98@kakao.com>
2023-05-20 06:02:42 +0900

    [Feat] UserService 클래스 생성
    - Services 디렉토리 생성

---
+++ server/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using server.DTOs;
+using server.Interface;
+
+namespace server.Services;
+
+public class UserService : IUserService
+{
+    public GetUserResponse Create()
+    {
+        return new GetUserResponse();
+    }
+    
+}
