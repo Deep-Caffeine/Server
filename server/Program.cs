8d9ce7871b0e9e283686c6ef2b118ed80f98bdd5
YeJun, Jung <yejun614@naver.com>
2023-05-20 22:02:16 +0900

    [Feat] ApplicationDbContext 의존성 주입

--- server/Program.cs
+++ server/Program.cs
@@ -1,8 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using server;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Dependency injection (services)
+builder.Services.AddDbContext<ApplicationDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
