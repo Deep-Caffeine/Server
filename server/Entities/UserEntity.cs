58f6141feaf42a0bf8e5253ccf11d84e92091c61
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-21 17:06:44 +0900

    [Feat] Email Attribute 추가

--- server/Entities/UserEntity.cs
+++ server/Entities/UserEntity.cs
@@ -1,17 +1,21 @@
-﻿namespace server.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Entities
 {
     public class UserEntity : BaseEntity
     {
+        [Required(ErrorMessage = "이메일을 입력하지 않았습니다.")]
+        [EmailAddress(ErrorMessage = "이메일 형식이 올바르지 않습니다.")]
         public string Email { get; set; }
-
+        
         public string Password { get; set; }
-
+        
         public string Username { get; set; }
-
+        
         public string Phone { get; set; }
-
+        
         public string Birth { get; set; }
-
+        
         public string ProfileURL { get; set; }
 
         public long Level { get; set; }
