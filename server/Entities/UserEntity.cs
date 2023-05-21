e645867e4c0e8a607e91073bcee7bd48a6ea6d68
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-05-21 17:08:27 +0900

    [Feat] Username Attribute 추가

--- server/Entities/UserEntity.cs
+++ server/Entities/UserEntity.cs
@@ -12,6 +12,8 @@ namespace server.Entities
         [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@#$%^&*()-=_+,.!?])(?!.*\s).{8,}$", ErrorMessage = "비밀번호는 8자 이상이어야 하며, 특수문자, 숫자, 알파벳이 반드시 포함되어야 합니다.")]
         public string Password { get; set; }
         
+        [Required(ErrorMessage = "사용자의 이름을 입력하지 않았습니다.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "사용자 이름의 최소 길이는 3자리 입니다.")]
         public string Username { get; set; }
         
         public string Phone { get; set; }
