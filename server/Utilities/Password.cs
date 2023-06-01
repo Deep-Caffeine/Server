51736306e6162c9b2e8b71c0219de41fce07b9fc
YeJun, Jung <yejun614@naver.com>
2023-06-02 01:49:25 +0900

    [Feat] 패스워드 생성용 매서드 추가

---
+++ server/Utilities/Password.cs
@@ -0,0 +1,21 @@
+﻿namespace server.Utilities;
+
+public static class Password
+{
+    public static string GenerateRandomPassword(int length = 15)
+    {
+        Random random = new Random();
+        string items = "ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklnmopqrstuvwxyz0123456789!@#$%^&*()_+=[]{}:;\'\",./<>?\\";
+
+        char[] password = new char[length];
+
+        for (int i = 0; i < length; ++i)
+        {
+            int index = random.Next(0, items.Length);
+            password[i] = items[index];
+        }
+
+        return new string(password);
+    }
+}
+
