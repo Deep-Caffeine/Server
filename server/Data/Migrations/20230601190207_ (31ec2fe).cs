8d0785af43a26d798576aaf70e8342367c661c52
Dotnet Migration Bot (by YeJun, Jung) <yejun614@naver.com>
2023-06-01 19:02:13 +0000

    [Chore] Auto EntityFramework Migrations

---
+++ server/Data/Migrations/20230601190207_ (31ec2fe).cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _31ec2fe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    Birth = table.Column<string>(type: "TEXT", nullable: false),
+                    ProfileURL = table.Column<string>(type: "TEXT", nullable: true),
+                    Level = table.Column<long>(type: "INTEGER", nullable: false),
+                    Sns = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
