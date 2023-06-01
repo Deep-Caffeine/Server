ccd99cac8d4f4bac2ba71f66b1e24c6ec9219a06
StarJune <partis98@kakao.com>
2023-06-02 05:02:08 +0900

    [Feat] Create User 요청 DTO 생성

---
+++ server/DTOs/CreateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.DTOs;
+
+public class CreateUserRequest
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Username { get; set; }
+    public string Phone { get; set; }
+    public string Birth { get; set; }
+}
