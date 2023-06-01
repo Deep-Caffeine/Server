35b4094e55dc2dbbd3bac098c48f4e26ae604166
StarJune <partis98@kakao.com>
2023-06-02 02:59:20 +0900

    [Fix] 배포 상태에 따른 사용할 DB를 다르게 하도록 변경

--- server/ApplicationDbContext.cs
+++ server/ApplicationDbContext.cs
@@ -7,17 +7,6 @@ namespace server
     {
         public DbSet<UserEntity> Users { get; set; }
 
-        public ApplicationDbContext()
-        {
-            var folder = Environment.SpecialFolder.LocalApplicationData;
-            var path = Environment.GetFolderPath(folder);
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            // FIXME: 이후 환경변수로 변경 필요
-            string connectionString = "Server=localhost;Database=deepcaffeine;Uid=root;Pwd=toor;";
-            optionsBuilder.UseSqlServer(connectionString);
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
 }
--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,6 @@
-﻿using server;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using server;
 using server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,12 +9,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Dependency injection (services)
-builder.Services.AddDbContext<ApplicationDbContext>();
+// builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddScoped<UserService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+if (builder.Environment.IsDevelopment())
+{
+    //개발 상일 경우 로컬의 SQLite로 연결
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        string DbPath = System.IO.Path.Join(path, "./data.db");
+        options.UseSqlite($"Data Source={DbPath}");
+    });
+}
+else
+{
+    //배포 상태인 경우 MySQL로 연결
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        //추후 환경변수로 변경 필요
+        string connectionString = "Server=localhost;Database=deepcaffeine;Uid=root;Pwd=toor;";
+        options.UseSqlServer(connectionString);
+    });
+}
 
 var app = builder.Build();
 
@@ -31,7 +55,7 @@ app.UseRouting();
 
 app.Use((context, next) =>
 {
-    context.Response.Headers["Access-Control-Allow-Origin"] = "*";  
+    context.Response.Headers["Access-Control-Allow-Origin"] = "*";
     context.Response.Headers["Access-Control-Allow-Header"] = "*";
     context.Response.Headers["Access-Control-Allow-Method"] = "*";
     return next.Invoke();
