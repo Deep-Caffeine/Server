a825f1908c6bb3eecfac087f1d10970b105c46cc
StarJune <112811200+98StarJune@users.noreply.github.com>
2023-06-02 04:59:13 +0900

    DbContext를 MySQL 사용으로 변경 (#28) (#36)
    * [Fix] DbContext 내 MySQL로 연결
    
    * [Fix] 배포 상태에 따른 사용할 DB를 다르게 하도록 변경
    
    * [Style] Auto dotnet format
    
    * [Fix] SQL서버 패키지를 MySQL 패키지로 변경
    
    * [Chore] Auto EntityFramework Migrations
    
    * [Feat] DB Connection String을 환경변수로부터 생성
    
    * [Style] Auto dotnet format
    
    * [Style] 불필요한 주석 삭제
    
    ---------
    
    Co-authored-by: Dotnet Format Bot (by YeJun, Jung) <yejun614@naver.com>
    Co-authored-by: YeJun, Jung <31740224+yejun614@users.noreply.github.com>

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,6 @@
-﻿using server;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using server;
 using server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,12 +9,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Dependency injection (services)
-builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddScoped<UserService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+if (builder.Environment.IsDevelopment())
+{
+    //개발 상일 경우 로컬의 SQLite로 연결
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        string DbPath = System.IO.Path.Join(path, "./data.db");
+        options.UseSqlite($"Data Source={DbPath}");
+    });
+}
+else
+{
+    //배포 상태인 경우 MySQL로 연결
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        string connectionString = $"Server={Environment.GetEnvironmentVariable("DB_HOST")};";
+        connectionString += $"Database={Environment.GetEnvironmentVariable("DB_NAME")};";
+        connectionString += $"Uid={Environment.GetEnvironmentVariable("DB_USER")};";
+        connectionString += $"Pwd={Environment.GetEnvironmentVariable("DB_PASSWORD")};";
+
+        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    });
+}
 
 var app = builder.Build();
 
