2fc79a02a0d12db3e74d1225053c1abac792c59c
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-06-02 03:14:40 +0900

    [Feat] PutUserRequest DTO 생성

---
+++ server/DTOs/PutUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace server.DTOs;
+
+public class PutUserRequest
+{
+    public string? username { get; set; }
+    public string? password { get; set; }
+    public string? phone { get; set; }
+    public string? birth { get; set; }
+}
