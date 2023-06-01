9aef6d53da69e9195976151c1cb6a550218ae3bc
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-06-02 04:31:58 +0900

    [Fix] password Validation ErrorMessage 제거

--- server/DTOs/PutUserRequest.cs
+++ server/DTOs/PutUserRequest.cs
@@ -7,7 +7,7 @@ public class PutUserRequest
 {
     [StringLength(20, MinimumLength = 3)]
     public string? username { get; set; }
-    [RegularExpression(@"^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "비밀번호는 8자 이상이어야 하며, 특수문자, 숫자, 알파벳이 반드시 포함되어야 합니다.")]
+    [RegularExpression(@"^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")]
     public string? password { get; set; }
     [Phone]
     public string? phone { get; set; }
