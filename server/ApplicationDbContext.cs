71def2779979ba38acad0173488b74338f5a7054
StarJune <partis98@kakao.com>
2023-06-02 02:17:20 +0900

    [Fix] DbContext 내 MySQL로 연결

--- server/ApplicationDbContext.cs
+++ server/ApplicationDbContext.cs
@@ -7,22 +7,17 @@ namespace server
     {
         public DbSet<UserEntity> Users { get; set; }
 
-        public string DbPath { get; }
-
         public ApplicationDbContext()
         {
             var folder = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(folder);
-
-            DbPath = System.IO.Path.Join(path, "./data.db");
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
-
-            // 로컬 Db와 연결
-            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+            // FIXME: 이후 환경변수로 변경 필요
+            string connectionString = "Server=localhost;Database=deepcaffeine;Uid=root;Pwd=toor;";
+            optionsBuilder.UseSqlServer(connectionString);
         }
     }
 }
