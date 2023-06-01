adcef0ce25c518e0af9efc1a72c4c880a39e419f
YeJun, Jung <yejun614@naver.com>
2023-06-02 06:30:05 +0900

    [Feat] JWT 기능 구현 2

---
+++ server/Attributes/RefreshAuthorizeAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace server.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class RefreshAuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // [AllowAnonymous] attribute가 있는 경우 스킵
+        bool allowAnonymouse = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
+
+        if (allowAnonymouse)
+        {
+            return;
+        }
+
+        // 토큰 인증 진행 (Refresh Token만 통과)
+        bool isValidation = (bool)(context.HttpContext.Items["IsValidation"] ?? false);
+        JwtSecurityToken? jwtToken = (JwtSecurityToken?)context.HttpContext.Items["JwtToken"];
+
+        if (!isValidation || jwtToken == null)
+        {
+            context.Result = new UnauthorizedResult();
+        }
+        else
+        {
+            Claim? isRefreshOnly = jwtToken.Claims.FirstOrDefault(x => x.Type == "refresh_only");
+
+            if (!(isRefreshOnly != null && isRefreshOnly.Value == "true"))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+    }
+}
+
---
+++ server/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Attributes;
+using server.DTOs;
+using server.Entities;
+using server.Services;
+
+namespace server.Controllers;
+
+[Route("auth")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly AuthService mAuthService;
+
+    public AuthController(AuthService authService)
+    {
+        mAuthService = authService;
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public ActionResult<AuthResponse> Auth([FromBody] AuthRequest authRequest)
+    {
+        UserEntity? userEntity = mAuthService.UserAuthorize(authRequest.email, authRequest.password);
+
+        AuthResponse response = new AuthResponse();
+
+        if (userEntity == null)
+        {
+            // Unauthorized
+            return Unauthorized();
+        }
+        else if (authRequest.no_token?? false)
+        {
+            // No token response
+            return response;
+        }
+        else
+        {
+            response.access_token = mAuthService.GenerateAccessToken(userEntity.Id);
+            response.refresh_token = mAuthService.GenerateRefreshToken(userEntity.Id);
+
+            return response;
+        }
+    }
+
+    [Route("refresh")]
+    [HttpGet]
+    [RefreshAuthorize]
+    public ActionResult<AuthResponse> Refresh()
+    {
+        JwtSecurityToken jwtToken = (JwtSecurityToken)HttpContext.Items["JwtToken"]!;
+        string id = jwtToken.Claims.First(x => x.Type == "id").Value;
+
+        AuthResponse response = new AuthResponse();
+        response.access_token = mAuthService.GenerateAccessToken(long.Parse(id));
+
+        return response;
+    }
+}
