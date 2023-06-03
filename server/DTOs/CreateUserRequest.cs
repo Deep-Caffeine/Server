07b701ae5eeb9264fd1cc1b744d55ee795e036b3
Dotnet Format Bot (by YeJun, Jung) <yejun614@naver.com>
2023-06-03 07:00:44 +0000

    [Style] Auto dotnet format

--- server/DTOs/CreateUserRequest.cs
+++ server/DTOs/CreateUserRequest.cs
@@ -6,16 +6,16 @@ public class CreateUserRequest
 {
     [EmailAddress]
     public string Email { get; set; }
-    
+
     [RegularExpression(@"^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")]
     public string Password { get; set; }
-    
+
     [StringLength(20, MinimumLength = 3)]
     public string Username { get; set; }
-    
+
     [Phone]
     public string Phone { get; set; }
-    
+
     [RegularExpression(@"^\d{4}-\d{2}-\d{2}$")]
     public string Birth { get; set; }
 }
