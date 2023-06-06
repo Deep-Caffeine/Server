661cf4d8bfc5e730ad011291f74acc45f6e26e62
Dotnet Format Bot (by YeJun, Jung) <yejun614@naver.com>
2023-06-06 08:24:51 +0000

    [Style] Auto dotnet format

--- server/Controllers/UserController.cs
+++ server/Controllers/UserController.cs
@@ -60,7 +60,9 @@ namespace server.Controllers
             {
                 return BadRequest(new
                 {
-                    status = 400, title = "This is a duplicate email.", errors = new { Email = "중복된 이메일 입니다." }
+                    status = 400,
+                    title = "This is a duplicate email.",
+                    errors = new { Email = "중복된 이메일 입니다." }
                 });
             }
         }
