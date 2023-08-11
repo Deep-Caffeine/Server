c26dacbcffde9eda5405de1ae25965d4d948436c
StarJune <partis98@kakao.com>
2023-08-12 01:40:40 +0900

    [Feat] ChatHttpService 객체 서비스 등록

--- server/Program.cs
+++ server/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddControllers();
 // Dependency injection (services)
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<ChatHttpService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
