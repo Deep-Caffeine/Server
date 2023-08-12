2875a66e7832ace994a7bc65da7955da55816596
StarJune <112811200+98StarJune@users.noreply.github.com>
2023-08-12 21:10:01 +0900

    채팅 관련 HTTP 개발을 위한 객체 생성 및 DI 처리 (#44) (#46)
    * [Feat] Chat 관련 HTTP Controller , Service 객체 생성
    
    * [Feat] DbContext 수정
    
    * [Feat] ChatHttpService 객체 서비스 등록
    
    * [Feat] Controller에 Service DI 처리
    
    * [Feat] Service 객체 내 Context DI 처리
    
    * [Style] Auto dotnet format
    
    ---------
    
    Co-authored-by: Dotnet Format Bot (by YeJun, Jung) <yejun614@naver.com>

--- server/ApplicationDbContext.cs
+++ server/ApplicationDbContext.cs
@@ -6,6 +6,9 @@ namespace server
     public class ApplicationDbContext : DbContext
     {
         public DbSet<UserEntity> Users { get; set; }
+        public DbSet<ChatLogsEntity> ChatLogsEntities { get; set; }
+        public DbSet<ChatParticipantsEntity> ChatParticipantsEntities { get; set; }
+        public DbSet<ChatRoomEntity> ChatRoomEntities { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
---
+++ server/Services/ChatHttpService.cs
@@ -0,0 +1,13 @@
+﻿using server.Interface;
+
+namespace server.Services;
+
+public class ChatHttpService : IChatHttpService
+{
+    private readonly ApplicationDbContext mContext;
+
+    public ChatHttpService(ApplicationDbContext context)
+    {
+        mContext = context;
+    }
+}
