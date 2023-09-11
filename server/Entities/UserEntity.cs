940a556ab50b5b3d4c129709b5158934b1ffbef7
Hun3431 <68728352+Hun3431@users.noreply.github.com>
2023-09-11 15:10:41 +0900

    [Fix] ErrorMessage 제거 (#31)
    Co-authored-by: StarJune <112811200+98StarJune@users.noreply.github.com>

--- server/Entities/UserEntity.cs
+++ server/Entities/UserEntity.cs
@@ -4,26 +4,26 @@ namespace server.Entities
 {
     public class UserEntity : BaseEntity
     {
-        [Required(ErrorMessage = "이메일을 입력하지 않았습니다.")]
-        [EmailAddress(ErrorMessage = "이메일 형식이 올바르지 않습니다.")]
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
-
-        [Required(ErrorMessage = "비밀번호를 입력하지 않았습니다.")]
-        [RegularExpression(@"^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "비밀번호는 8자 이상이어야 하며, 특수문자, 숫자, 알파벳이 반드시 포함되어야 합니다.")]
+        
+        [Required]
+        [RegularExpression(@"^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")]
         public string Password { get; set; }
-
-        [Required(ErrorMessage = "사용자의 이름을 입력하지 않았습니다.")]
-        [StringLength(20, MinimumLength = 3, ErrorMessage = "사용자 이름의 최소 길이는 3자리 입니다.")]
+        
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
         public string Username { get; set; }
-
-        [Required(ErrorMessage = "휴대폰 번호를 입력하지 않았습니다.")]
-        [Phone(ErrorMessage = "휴대폰 번호 형식이 올바르지 않습니다.")]
+        
+        [Required]
+        [Phone]
         public string Phone { get; set; }
-
-        [Required(ErrorMessage = "생년월일을 입력하지 않았습니다.")]
-        [RegularExpression(@"^\d{4}-\d{2}-\d{2}$", ErrorMessage = "생년월일의 형식이 올바르지 않습니다.")]
-        public DateTime Birth { get; set; }
-
+        
+        [Required]
+        [RegularExpression(@"^\d{4}-\d{2}-\d{2}$")]
+        public string Birth { get; set; }
+        
         public string? ProfileURL { get; set; }
 
         public long Level { get; set; }
