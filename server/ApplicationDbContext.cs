f3d08842468877fbdbd80dc5073a206e078e96d0
yejin-e <yejin01e@gmail.com>
2023-10-11 01:58:59 +0900

    [Feat] SchoolInformation Service 추가

--- server/ApplicationDbContext.cs
+++ server/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace server
         public DbSet<ChatLogsEntity> ChatLogsEntities { get; set; }
         public DbSet<ChatParticipantsEntity> ChatParticipantsEntities { get; set; }
         public DbSet<ChatRoomEntity> ChatRoomEntities { get; set; }
-
+        public DbSet<SchoolInformationEntity> SchoolInformationEntities { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
