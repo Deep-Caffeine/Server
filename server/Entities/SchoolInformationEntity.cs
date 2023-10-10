a26c73655cd5d26110b053287d0fd53d3e11007a
yejin-e <yejin01e@gmail.com>
2023-10-11 01:38:07 +0900

    [Feat] 학교 정보 저장 Entity 추가

---
+++ server/Entities/SchoolInformationEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Entities
+{
+    public class SchoolInformationEntity : BaseEntity
+    {
+        [Required]
+        public long User { get; set; }
+
+        [Required]
+        public string School { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        public int? Grade { get; set; }
+        
+    }
+}
