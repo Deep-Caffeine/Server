9c2d6c990d43622dbf5be7df1256ea0370d3cc4f
yejin-e <yejin01e@gmail.com>
2023-10-11 01:39:07 +0900

    [Feat] 학교 정보 추가 Request DTO 추가

---
+++ server/DTOs/CreateSchoolRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.DTOs;
+
+public class CreateSchoolRequest
+{
+    [Required] public string School { get; set; }
+
+    [Required] public string Department { get; set; }
+
+    [Required] public string State { get; set; }
+
+    public int? Grade { get; set; }
+}
