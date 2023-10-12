6a252714a80c948eea6bd73c7dc1784746b61e8c
yejin-e <yejin01e@gmail.com>
2023-10-12 21:42:28 +0900

    [Style] 코드 줄 바꿈

--- server/DTOs/CreateSchoolRequest.cs
+++ server/DTOs/CreateSchoolRequest.cs
@@ -4,11 +4,14 @@ namespace server.DTOs;
 
 public class CreateSchoolRequest
 {
-    [Required] public string School { get; set; }
+    [Required] 
+    public string School { get; set; }
 
-    [Required] public string Department { get; set; }
+    [Required] 
+    public string Department { get; set; }
 
-    [Required] public string State { get; set; }
+    [Required] 
+    public string State { get; set; }
 
     public int? Grade { get; set; }
 }
